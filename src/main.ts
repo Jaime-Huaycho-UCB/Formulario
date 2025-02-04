344af7370efcae8b0cfbedf38c96a19b96b77a38
Jaime <huaychojaime@gmail.com>
2025-02-04 12:02:54 -0400

    adicion del corrs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { listeners } from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    listeners: '*'
+  })
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
